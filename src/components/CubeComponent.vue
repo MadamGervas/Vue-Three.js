7bd861df4c88d6d013c0513bf0c8ee7e9d2d34c7
dgervas <Daria.Gervas@noveogroup.com>
2025-12-29 23:56:48 +0700

    test

---
+++ src/components/CubeComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <TresMesh ref="cubeRef" :position="[0, 2, 0]">
+    <TresBoxGeometry :args="[1, 1, 1]" />
+    <TresMeshNormalMaterial />
+  </TresMesh>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import { useLoop } from "@tresjs/core";
+
+const { onBeforeRender } = useLoop();
+const cubeRef = ref();
+
+onBeforeRender(({ delta, elapsed }) => {
+  if (cubeRef.value) {
+    cubeRef.value.rotation.y += delta;
+    cubeRef.value.rotation.z = elapsed;
+  }
+});
+</script>
---
+++ src/components/TheExperience.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import CubeComponent from "./CubeComponent.vue";
+</script>
+
+<template>
+  <TresCanvas clear-color="#FFC0CB" window-size>
+    <TresPerspectiveCamera
+      :args="[75, 1, 0.1, 1000]"
+      :position="[3, 1, 4]"
+      :look-at="[0, 0, 0]"
+    />
+
+    <TresScene>
+      <CubeComponent />
+    </TresScene>
+    <TresAxesHelper />
+  </TresCanvas>
+</template>
